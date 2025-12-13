35fd7e3dea687a3663d724325c4653091a02d4af
AcordLOL <abenedictprovido@gmail.com>
2025-12-13 15:53:27 +0800

    test 1

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
-
 const port = 8010
+
+dotenv.config();
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("So... You want songs don't you?");
+})
+
+app.get("/getsongs/:type/:id", async(req, res) => {
+    try {
+        const type = req.params.type;
+        const id = req.params.id;
+        
+        res.send('sup');
+    }catch (e) {
+        console.log('Error:', e);
+        res.status(500).send('Internal Server Error when getting songs');
+    }
 })
 
 app.listen(port, () => {
