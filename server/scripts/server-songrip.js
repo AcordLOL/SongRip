df9097910aa4e1853a7dbe63ac9a836b93edde87
AcordLOL <abenedictprovido@gmail.com>
2025-12-13 17:13:04 +0800

    dot env issues

--- server/scripts/server-songrip.js
+++ server/scripts/server-songrip.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import search from 'yt-search';
 import { promisify } from 'node:util';
 import { exec } from "node:child_process";
