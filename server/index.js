49d983327ed2c6ab25b758f80d8cff05cba4d915
AcordLOL <abenedictprovido@gmail.com>
2025-12-13 15:59:47 +0800

    another

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"
+
+dotenv.config();
 
 const app = express();
 const port = 8010
 
-dotenv.config();
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: ["https://song-rip.vercel.app", `http://localhost:${port}`]
+}))
 
 app.get("/", (req, res) => {
     res.send("So... You want songs don't you?");
