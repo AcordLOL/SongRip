bcb0eb16177ebb72c0ed89b17276c682d475af0f
AcordLOL <abenedictprovido@gmail.com>
2025-12-13 15:22:52 +0800

    server reset ig

--- server/index.js
+++ server/index.js
@@ -1,43 +1,13 @@
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-
-import SSR from "./scripts/server-songrip.js";
 
 const app = express();
-app.use(express.json({ limit: '10mb' }))
-app.use(express.urlencoded({ extended: true })); 
 
-app.use(cors({
-    origin: ["http://localhost:5173"]
-}))
+const port = 8010
 
-dotenv.config();
-
-app.get("/dl-s-:plat/:type/:id", async (req, res) => {
-    try{
-        const platform = req.params.plat;
-        const type = req.params.type;
-        const id = req.params.id;
-
-        res.send(await SSR.getSongs(id, type));
-    }catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-app.post("/download/:song_name", async (req, res) => { 
-    try{
-        const file = await SSR.downloadFile(req.body)
-        
-        res.sendFile(file)
-    }catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
-    }
+app.get("/", (req, res) => {
+    res.send("So... You want songs don't you?");
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`listening at: https://localhost:${process.env.PORT}`)
+app.listen(port, () => {
+    `Server started on port ${port}`
 })
